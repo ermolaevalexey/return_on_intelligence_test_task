896abdeab9ba7dc183e3d936f87aca5caae6ea95
Алексей Ермолаев <aleksejermolaev@MacBook-Pro-Aleksej.local>
2016-08-01 17:50:30 +0300

    added more comments to js code

--- src/js/main.js
+++ src/js/main.js
@@ -17,12 +17,14 @@ $(document).ready(function () {
 			$input = $(this);
 		$input.parent().addClass('editing');
 
+		//We typed nothing and focus is broken
 		$input.bind('keyup keydown', function() {
 			if ($input.html() === '' && !$input.is(':focus')){
 				$input.append(textContent);
 			}
 		});
 
+		//Applying new value
 		$input.parent().find('.edit-accept').click(function (e) {
 			e.preventDefault();
 			var newContent = $input.html() || textContent;
@@ -30,6 +32,7 @@ $(document).ready(function () {
 			$input.parent().removeClass('editing');
 		});
 
+		//Cancelling typing and return previous value
 		$input.parent().find('.edit-decline').click(function (e) {
 			e.preventDefault();
 			var oldContent = textContent;
@@ -39,23 +42,30 @@ $(document).ready(function () {
 
 	});
 
+	//Add skills field functionality
+
 	$('#user-add-skills').bind('focus', function () {
 		var textContent = $(this).html(),
 			$input = $(this),
-			$skillLevel = $input.parent().find('#skill-level').val();
+			//Remember default value of skill level
+			$skillLevel = $input.parent().find('#skill-level').val(); 
 
 		$input.parent().addClass('editing');
 
+		//Applying new value of skill level
 		$input.parent().find('#skill-level').on('change', function () {
 			$skillLevel = $(this).val();
 		});
 
+		//Trying to append skill
 		$input.parent().find('.edit-accept').click(function (e) {
 			e.preventDefault();
+			//We typed nothing and focus is broken or we've got default placeholder value
 			if ($input.html() === '' && !$input.is(':focus') || $input.html() === textContent){
 				$input.parent().removeClass('editing');
 				return;
 			} else {
+				//Appending new li element to list
 				var li = '<li class="user-skill-item ' + $skillLevel + '">'+ $input.html() 
 						 	+ '<a href="#" class="delete-skill"></a>' + 
 						 '</li>';
@@ -67,6 +77,7 @@ $(document).ready(function () {
 
 	});
 
+	//Removing li element from list
 	$('.user-skill-item').find('.delete-skill').click(function (e) {
 		e.preventDefault();
 		$(this).parent().remove();
